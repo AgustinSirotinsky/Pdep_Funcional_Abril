6c7298ac325f82387f019e70030f27f40b23a6cc
Agustin Sirotinsky <agussiro06@gmail.com>
2026-04-22 17:40:38 -0300

    Llegue a meter todo el punto 1 y parte del punto 2, despues sigo pasa que tiago quiere programar

--- file.hs
+++ file.hs
@@ -1,3 +1,41 @@
-text :: String
+-- Punto 1
+-- Modelar las siguientes sustancias:
+-- a. El hidrógeno y el oxígeno
+-- b. El agua, sustancia compuesta por 2 hidrógenos y 1 oxígeno.
 
-text = "commit inicial"
+--                       
+data Elemento = Elemento Int String String String -- Valor atomico, Nombre, Letra con la que se le identifica lol, Tipo  
+    deriving (Show, Eq)
+hidrogeno = Elemento 1 "Hidrogeno" "H" "No metal"
+oxigeno = Elemento 8 "Oxigeno" "O" "No metal"
+
+data Sustancia = Sustancia String String [(Elemento, Int)] -- Nombre, Tipo, [Elemento, cantidad de elemento]
+    deriving (Show, Eq)
+agua = Sustancia "Agua" "No metal" [(hidrogeno, 2), (oxigeno, 1)]
+
+
+
+--Punto 2
+-- Poder saber si una sustancia conduce bien según un criterio. Los criterios actuales son electricidad y calor, pero podría haber más. Los metales conducen bien cualquier criterio, sean compuestos o elementos. Los elementos que sean gases nobles conducen bien a la electricidad, y los compuestos halógenos conducen bien el calor. Para el resto, no son buenos conductores.
+
+--Punto 3
+-- Obtener el nombre de unión de un elemento. Esto se logra añadiendo "uro" al final del nombre, pero solo si el nombre termina en consonante. Si termina en vocal, se busca hasta la última consonante y luego sí, se le concatena "uro". Por ejemplo, el nombre de unión del Flúor es "fluoruro", mientras que el nombre de unión del mercurio es "mercururo".
+
+--Punto 4
+-- Combinar 2 nombres. Al nombre de unión del primero lo concatenamos con el segundo, agregando un "de" entre medio. Por ejemplo, si combino "cloro" y "sodio" debería obtener "cloruro de sodio".
+
+--Punto 5
+-- Mezclar una serie de componentes entre sí. El resultado de dicha mezcla será un compuesto. Sus componentes serán los componentes mezclados. El nombre se forma de combinar los nombres de la sustancia de cada componente. La especie será, arbitrariamente, un no metal.
+
+--Punto 6
+-- Obtener la fórmula de una sustancia:
+-- para los elementos es su símbolo químico
+-- para los compuestos es la concatenación de las representaciones de sus componentes y se pone entre paréntesis
+
+-- La representación de un componente depende de la cantidad de moléculas:
+
+-- si tiene una, entonces solo es la fórmula de su sustancia
+-- si tiene más, entonces es la fórmula de su sustancia y la cantidad
+
+-- Por ejemplo, la fórmula del agua debería ser (H2O). ¡Recuerden que una sustancia compuesta puede estar compuesta por otras sustancias compuestas!
+
