e7064e0bd8a581d579c53424978535d74ca80eeb
Agustin Sirotinsky <agussiro06@gmail.com>
2026-04-22 18:17:46 -0300

    punto 2 listo y meti algo q m tiro vicky

--- file.hs
+++ file.hs
@@ -3,14 +3,19 @@
 -- a. El hidrógeno y el oxígeno
 -- b. El agua, sustancia compuesta por 2 hidrógenos y 1 oxígeno.
 
-data Elemento = Elemento Int String String String -- Valor atomico, Nombre, Letra con la que se le identifica lol, Tipo  
+data Sustancia
+    = Elemento Int String String String -- Valor atomico, Nombre, Letra con la que se le identifica lol, Tipo
+    | Compuesto String String [(Sustancia, Int)] -- Nombre, Tipo, [Elemento, cantidad de elemento]
     deriving (Show, Eq)
+
 hidrogeno = Elemento 1 "Hidrogeno" "H" "No metal"
 oxigeno = Elemento 8 "Oxigeno" "O" "No metal"
+oro = Elemento 79 "Oro" "Au" "Metal"
+yodo = Elemento 53 "Yodo" "I" "Halogeno"
+xenon = Elemento 54 "Xenon" "Xe" "Gas noble"
 
-data Sustancia = Sustancia String String [(Elemento, Int)] -- Nombre, Tipo, [Elemento, cantidad de elemento]
-    deriving (Show, Eq)
-agua = Sustancia "Agua" "No metal" [(hidrogeno, 2), (oxigeno, 1)]
+agua = Compuesto "Agua" "No metal" [(hidrogeno, 2), (oxigeno, 1)]
+metal = Compuesto "Metal" "Metal" [(hidrogeno, 2), (oxigeno, 1)]
 
 -- Punto 2
 -- Poder saber si una sustancia conduce bien según un criterio. Los criterios actuales son electricidad y calor, pero podría haber más. 
@@ -18,14 +23,19 @@ agua = Sustancia "Agua" "No metal" [(hidrogeno, 2), (oxigeno, 1)]
 -- Los elementos que sean gases nobles conducen bien a la electricidad, y los compuestos halógenos conducen bien el calor. 
 -- Para el resto, no son buenos conductores.
 
-conduceBien :: Sustancia -> Bool -- True si conduce bien y False si no
-conduceBien (Sustancia _ _ _ "Metal") = True
-conduceBien (Elemento _ _ _ "Metal") = True
-conduceBien "electricidad" (Elemento _ _ _ "Metal") = True
+conduceBien :: String -> Sustancia -> Bool -- True si conduce bien y False si no
+conduceBien _ (Compuesto _ "Metal" _) = True
+conduceBien _ (Elemento _ _ _ "Metal") = True
+conduceBien "electricidad" (Elemento _ _ _ "Gas noble") = True
+conduceBien "calor" (Compuesto _ "Halogeno" _) = True
+conduceBien _ _ = False
 
 
 --Punto 3
 -- Obtener el nombre de unión de un elemento. Esto se logra añadiendo "uro" al final del nombre, pero solo si el nombre termina en consonante. Si termina en vocal, se busca hasta la última consonante y luego sí, se le concatena "uro". Por ejemplo, el nombre de unión del Flúor es "fluoruro", mientras que el nombre de unión del mercurio es "mercururo".
+
+dropWhile --dice vicky
+-- funcion que recibe una condicion y una lista
 
 --Punto 4
 -- Combinar 2 nombres. Al nombre de unión del primero lo concatenamos con el segundo, agregando un "de" entre medio. Por ejemplo, si combino "cloro" y "sodio" debería obtener "cloruro de sodio".
@@ -44,4 +54,3 @@ conduceBien "electricidad" (Elemento _ _ _ "Metal") = True
 -- si tiene más, entonces es la fórmula de su sustancia y la cantidad
 
 -- Por ejemplo, la fórmula del agua debería ser (H2O). ¡Recuerden que una sustancia compuesta puede estar compuesta por otras sustancias compuestas!
-
